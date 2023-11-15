b768a3540569965a9f2c753e4645cc0a0cef1cb1
ofomata <miracleosita355@gmail.com>
2023-11-15 23:24:56 +0100

    no changes

---
+++ script.js
@@ -0,0 +1,73 @@
+//JavaScript to fetch images from unsplash using api
+
+const accessKey = "5FRLuqOgdKn51gkr7P1HaUYo9WkdNmv7F_xQMXPCsxw";
+
+const formElement = document.querySelector("form");
+const inputElement = document.getElementById("search-image");
+const searchResult = document.querySelector(".search-result");
+const moreButton = document.getElementById("more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImage() {
+    inputData = inputElement.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-results");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResult.appendChild(imageWrapper);
+    });
+
+    page++
+    if (page > 1) {
+        moreButton.style.display = "block";
+    }
+}
+
+formElement.addEventListener("submit", (event) =>{
+event.preventDefault();
+page = 1;
+searchImage();
+});
+
+moreButton.addEventListener("click", () =>{
+    searchImage();
+});
+
+
+
+//JavaScript to contol the navigation bar
+
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+    nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+    nav.classList.remove("open-nav");
+});
+
