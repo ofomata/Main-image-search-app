3c2bfb4f0b47dccab14c861893e3adc4d5d96838
ofomata <miracleosita355@gmail.com>
2023-11-16 17:29:57 +0100

    updated the code to add a dark and light mood

--- script.js
+++ script.js
@@ -71,3 +71,31 @@ close.addEventListener("click", () => {
     nav.classList.remove("open-nav");
 });
 
+//JavaScript for the dark mode button
+
+const darkMode = document.getElementById("dark-btn");
+
+darkMode.onclick = function() {
+    darkMode.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") === "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+if (localStorage.getItem("theme") === "light") {
+    darkMode.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if (localStorage.getItem("theme") === "dark") {
+    darkMode.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else {
+    localStorage.setItem("theme", "light");
+}
+
